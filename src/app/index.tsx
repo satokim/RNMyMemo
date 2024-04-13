08bf78956c975ba9f0a38de187770df1908a8942
satokim <usksty@gmail.com>
2024-04-13 23:31:20 +0900

    Create Index style sheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,40 +2,41 @@ import { View, Text, StyleSheet } from 'react-native'
 
 const Index = (): JSX.Element => {
     return (
-        <View style = { styles.container }>
+        <View style={styles.container}>
 
-            <View>
-                <View>
-                    <Text>RNMyMemo</Text>
-                    <Text>Logout</Text>
+            <View style={styles.header}>
+                <View style={styles.headerInner}>
+                    <Text style={styles.headerTitle}>RNMyMemo</Text>
+                    <Text style={styles.headerRight}>Logout</Text>
                 </View>
             </View>
 
             <View>
-                <View>
+
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>Buy list</Text>
-                        <Text>2023.10.01 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>Buy list</Text>
+                        <Text style={styles.memoListItemDate}>2023.10.01 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>Buy list</Text>
-                        <Text>2023.10.01 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>Buy list</Text>
+                        <Text style={styles.memoListItemDate}>2023.10.01 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
                     </View>
                 </View>
 
-                <View>
+                <View style={styles.memoListItem}>
                     <View>
-                        <Text>Buy list</Text>
-                        <Text>2023.10.01 10:00</Text>
+                        <Text style={styles.memoListItemTitle}>Buy list</Text>
+                        <Text style={styles.memoListItemDate}>2023.10.01 10:00</Text>
                     </View>
                     <View>
                         <Text>X</Text>
@@ -44,8 +45,8 @@ const Index = (): JSX.Element => {
 
             </View>
 
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style={styles.circleButtonLabel}>+</Text>
             </View>
         </View>
     )
@@ -53,11 +54,82 @@ const Index = (): JSX.Element => {
 
 const styles = StyleSheet.create({
     container: {
+        // Flex:1 は画面全体を選択状態
         flex: 1,
-        // ReactNativeでは縦方向,Webでは横方向
-        justifyContent: 'center',
-        // ReactNativeでは横方向,Webでは縦方向
+        // // ReactNativeでは縦方向,Webでは横方向
+        // justifyContent: 'center',
+        // // ReactNativeでは横方向,Webでは縦方向
+        // alignItems: 'center'
+        backgroundColor: '#ffffff'
+    },
+    header: {
+        backgroundColor: '#467FD3',
+        height: 104,
+        justifyContent: 'flex-end'
+    },
+    headerInner: {
         alignItems: 'center'
+    },
+    headerRight: {
+        position: 'absolute',
+        right: 16,
+        bottom: 16,
+        color: 'rgba(255,255,255,0.7)'
+    },
+    headerTitle: {
+        marginBottom: 8,
+        fontSize: 22,
+        lineHeight: 32,
+        fontWeight: 'bold',
+        color: '#ffffff'
+    },
+    memoListItem: {
+        backgroundColor: '#ffffff',
+        // Defaultは縦方向なので横方向に指定。flexDirectionはDefaultの位置を指定する。
+        // ReactNativeでは縦方向 -> 横方向、Webでは横方向 -> 縦方向
+        // justifyContent: 'center',
+        // ReactNativeでは横方向 -> 縦方向、Webでは縦方向 -> 横方向
+        // alignItems: 'center'
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 16,
+        paddingHorizontal: 19,
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: 'rgba(0,0,0,0.15)'
+    },
+    memoListItemTitle: {
+        fontSize: 16,
+        lineHeight: 32
+    },
+    memoListItemDate: {
+        fontSize: 12,
+        lineHeight: 16,
+        color: '#848484'
+    },
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#467FD3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 40,
+        bottom: 40,
+        // iOS shadow effect
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: { width: 0, height: 8 },
+        // AOS shadow effect
+        // 高さの位置
+        elevation: 8
+    },
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 40
     }
 })
 
